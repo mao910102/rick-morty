9002145ae076cf47c066796176b33a5fdb23f2ed
mao910102 <69535836+mao910102@users.noreply.github.com>
2022-05-19 18:56:56 -0500

    flujo inicial app

---
+++ src/app/components/pages/characters/characters.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CharactersComponent } from './characters.component';
+
+
+
+@NgModule({
+  declarations: [
+    CharactersComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CharactersModule { }
